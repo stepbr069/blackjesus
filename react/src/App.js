48a662dceb6052d847bbdef62d18fd29bb34ecf7
stepbr069 <153720557+stepbr069@users.noreply.github.com>
2023-12-13 14:54:35 +0530

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginSignup from './components/LoginSignup';
+
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
